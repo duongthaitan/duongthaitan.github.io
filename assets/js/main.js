6f1a8c0cff41926016727b23f873a92e4b6c1f34
TANHIPP\duong <duongthaitan13@gmail.com>
2024-09-27 20:43:15 +0700

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,16 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+window.onloadTurnstileCallback = function () {
+  turnstile.render("#myCapcha", {
+    sitekey: "<0x4AAAAAAAmlq1HcTM6FEnDv>",
+    callback: function (token) {
+      console.log(`Challenge Success ${token}`);
+    },
+  });
+};
+
 (function() {
   "use strict";
 
