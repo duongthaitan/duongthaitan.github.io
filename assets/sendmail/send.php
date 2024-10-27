446c6b9d8cc3e4656b96a93c62eb6c7c957106c2
TANHIPP\duong <duongthaitan13@gmail.com>
2024-10-28 02:05:41 +0700

    Done

--- assets/sendmail/send.php
+++ assets/sendmail/send.php
@@ -6,18 +6,14 @@ use PHPMailer\PHPMailer\Exception;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Kiểm tra phương thức POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Include PHPMailer
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/PHPMailer.php';
     require 'PHPMailer/src/SMTP.php';
 
-    // Khởi tạo PHPMailer
     $mail = new PHPMailer(true);
 
     try {
-        // Cấu hình SMTP
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
@@ -26,24 +22,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->Port = 587;
         $mail->SMTPSecure = 'tls';
 
-        // Thiết lập thông tin người gửi và người nhận
         $mail->setFrom('duongthaitan13@gmail.com', 'Duong Thai Tan');
-        $mail->addAddress('duongthaitan13@gmail.com'); // Người nhận
+        $mail->addAddress('duongthaitan13@gmail.com');
 
-        // Thiết lập nội dung email
-        $mail->isHTML(false); // Gửi email dạng text
-        $mail->Subject = $_POST['subject']; // Tiêu đề từ form
+        $mail->isHTML(false);
+        $mail->Subject = $_POST['subject'];
         $mail->Body    = "Name: " . $_POST['name'] . "\n" . "Email: " . $_POST['email'] . "\n" . "Message: " . $_POST['message'];
 
-        // Gửi email
         $mail->send();
         echo '<script>alert("Email đã được gửi thành công.");</script>';
     } catch (Exception $e) {
-        echo '<script>alert("Email không thể gửi. Vui lòng thử lại.");</script>';
+        echo '<script>alert("Email không thể gửi. Lỗi: ' . $e->getMessage() . '");</script>';
     }
 } else {
     http_response_code(405);
     echo 'Method Not Allowed. Please ensure you are using POST method.';
 }
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
